21748a55f5e89f891bf2387c78eb8da95d41e63e
Monica Foreshaw <monicaforeshaw6@gmail.com>
2024-11-21 15:25:04 -0500

    Created Register and Login page using identity

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Bank_System.Startup))]
+namespace Online_Bank_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
